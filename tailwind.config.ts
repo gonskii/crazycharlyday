f159bec3f391f1fe2ad846c0364e0ac65869ab36
Delioos <htz_j@protonmail.com>
2024-02-22 10:25:07 +0100

    build(next): fuck arthur

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
+import typographyPlugin from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme.js';
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
+const config = {
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+	darkMode: ['class'],
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-inter)', ...fontFamily.sans],
+			},
+			transitionTimingFunction: {
+				easeInOutQuart: 'cubic-bezier(0.77, 0, 0.175, 1)',
+			},
+		},
+	},
+	plugins: [typographyPlugin],
+} satisfies Config;
+
 export default config;
